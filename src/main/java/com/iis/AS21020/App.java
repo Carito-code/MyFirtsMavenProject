aa6d7a74988129c098cfb57c75fee586268e52a4
Carito <as21020@ues.edu.sv>
2025-05-11 13:08:32 -0600

    Hola este es un proyecto de prueba sobre Maven y Jenkins

---
+++ src/main/java/com/iis/AS21020/App.java
@@ -0,0 +1,15 @@
+package com.iis.AS21020;
+
+/**
+ * Hello world!
+ */
+public class App {
+	
+	public String Hello() {
+		return "HOlaaaa Mundo";
+	}
+    public static void main(String[] args) {
+    	App hw = new App();
+        System.out.println(hw.Hello());
+    }
+}
